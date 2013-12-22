bb2e72a91cc21164ffe6ce75dce12f743642353d
treby <treby@atelier-nodoka.net>
2013-12-22 12:23:29 +0900

    Included sp.js and wrote app description.

--- js/main.js
+++ js/main.js
@@ -1,50 +1,51 @@
 window.onload = function onWindowLoaded () {
     enchant();
-    
+
     var game = new Game(320, 320);
     game.fps = 15;
-    
     var logger = new jam.Logger({console: console});
-    
     var app = new jam.Application({game: game, logger: logger});
-        
     var topLevel = new jam.Level({
         name: 'top',
         preload: function (context) {
+            context.app.log(context.game);
             context.app.log("Preloading topLevel...");
-            context.game.preload('http://jsrun.it/assets/l/4/0/X/l40Xg.png');
+            context.game.preload('img/mikoto.png');
         },
         load: function (context, params) {
-            var game = context.game,
-                    app = context.app;
-            
+            var game = context.game;
+            var app = context.app;
+
             app.log("params=", params, "context=", context);
-            
+
             var scene = (function () {
-                var label1 = new Label('hoge' + jam.guid());
+                var label1 = new Label('ああ' + jam.guid());
                 label1.x = 50;
                 label1.y = 100;
                 label1.color = '#ff0000';
-                label1.font = '8px cursive';
+                label1.font = '12px';
                 label1.opacity = 0.8;
                 label1.text += 'fuga';
-                
+
                 var sprite = new Sprite(32, 32);
-                sprite.image = game.assets['http://jsrun.it/assets/l/4/0/X/l40Xg.png'];
+                sprite.image = game.assets['img/mikoto.png'];
+                sprite.x = 50;
+                sprite.y = 50;
                 sprite.addEventListener('touchstart', function () {
+                    app.log(window);
                     window.alert("test");
                     app.loadLevel('two', {foo:"bar"});
                 });
                 sprite.addEventListener('touchend', function () {
                 });
-                
+
                 // use a Canvas element as a Sprite's drawing element
-                var sprite2 = new Sprite(64, 64);
-                var surface = new Surface(320, 320);
-                sprite2.image = surface;
-                sprite2.x = 64;
-                sprite2.y = 128;
-                
+                //var sprite2 = new Sprite(64, 64);
+                //var surface = new Surface(320, 320);
+                //sprite2.image = surface;
+                //sprite2.x = 64;
+                //sprite2.y = 128;
+
                 var scene = new Scene();
                 scene.backgroundColor = '#eeeeee';
                 scene.addEventListener('touchstart', function(e) {
@@ -55,22 +56,22 @@ window.onload = function onWindowLoaded () {
                 });
 
                 scene.addChild(sprite);
-                scene.addChild(sprite2);
+                //scene.addChild(sprite2);
                 scene.addChild(label1);
-                
+
                 return scene;
             })();
-            
+
             var frameCount = 0;
             this.onEnterFrame = function () {
                 frameCount ++;
-                
+
                 if (frameCount % 100 == 0) {
                     context.app.log(frameCount + " frames has passed until now.");
                 }
             };
             game.addEventListener('enterframe', this.onEnterFrame);
-            
+
             return { scene: scene };
         },
         unload: function (context) {
@@ -79,7 +80,7 @@ window.onload = function onWindowLoaded () {
             this.onEnterFrame = null;
         }
     });
-    
+
     var levelTwo = new jam.Level({
         name: "two",
         load: function (context, params) {
